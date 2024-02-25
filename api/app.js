6410b96bdc2be9ff17b712e1a1ff25c041661550
Adonias <adoniashaile1@gmail.com>
2024-02-25 17:25:31 +0300

    using volume in docker containers

--- api/app.js
+++ api/app.js
@@ -9,19 +9,20 @@ app.get('/', (req, res) => {
   res.json([
     {
       "id":"1",
+      "new":"nodemon using volume",
       "title":"Book Review: The Bear & The Nightingale"
     },
     {
       "id":"2",
-      "title":"Game Review: Pokemon Brillian Diamond"
+      "title":"Review: Pokemon Brillian Diamond"
     },
     {
       "id":"3",
-      "title":"Show Review: Alice in Borderland"
+      "title":"Review: Alice in Borderland"
     }
   ])
 })
 
 app.listen(4000, () => {
-  console.log('listening for requests on port 4000')
+  console.log('listening for requests on port 4000...')
 })
